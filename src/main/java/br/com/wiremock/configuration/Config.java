acb5d720751275adace50b521e54e5f884ed7534
Everson Gouvea <suporte.gouvea@gmail.com>
2019-11-12 17:13:25 -0300

    Criando redirecionamento para server wireMock

--- src/main/java/br/com/wiremock/configuration/Config.java
+++ src/main/java/br/com/wiremock/configuration/Config.java
@@ -5,27 +5,30 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
 @Configuration
 public class Config {
-	
+
 	@Value("${wiremock.port:8081}")
 	private int port;
-	
+
 	@Value("${wiremock.mappings:src/test/resources}")
 	private String mappings;
-	
-	
+
 	@Bean
 	public WireMockServer createWireMock() {
 
-		WireMockConfiguration configWireMoc = wireMockConfig().port(port)
-				                                              .usingFilesUnderDirectory(mappings);
+		WireMockConfiguration configWireMoc = wireMockConfig().port(port).usingFilesUnderDirectory(mappings);
 		return new WireMockServer(configWireMoc);
-		
+
 	}
-	
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/br/com/wiremock/controller/WireMockController.java
+++ src/main/java/br/com/wiremock/controller/WireMockController.java
@@ -1,7 +1,6 @@
 package br.com.wiremock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 @RestController
-@RequestMapping(value = "wiremock")
+@RequestMapping(value = "wiremock-services")
 public class WireMockController {
 
 	@Autowired
 	WireMockServer wire;
-	
-	@Value("${wiremock.port:8081}")
-	private int port;
 	
 	@GetMapping(path = "/start")
 	public String start() {
 		
 		wire.start();
 		wire.resetToDefaultMappings();
-		return  String.format("Iniciando wireMock port: %d", port) ;
+		return  String.format("Iniciando wireMock na porta: %d - Url alternativa: http://hots/wiremock", wire.port()) ;
 		
 	}
 	
